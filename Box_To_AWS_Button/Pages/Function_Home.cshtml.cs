3467d6ecf3ca657123db5ff21b7b2ff93f95c50d
shivambhuyarkardz1 <Shivam.Bhuyarkar@datazenengineering.com>
2023-12-11 14:52:59 +0530

    Add project files.

---
+++ Box_To_AWS_Button/Pages/Function_Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Box_To_AWS_Button.Pages
+{
+    public class Function_HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
