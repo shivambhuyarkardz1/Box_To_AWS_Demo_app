df63fcd8a510f26deb0e14112c28b772523f1e0a
shivambhuyarkardz1 <Shivam.Bhuyarkar@datazenengineering.com>
2023-12-12 10:03:31 +0530

    changes

--- Box_To_AWS_Button/Pages/box.cshtml.cs
+++ Box_To_AWS_Button/Pages/box.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Box_To_AWS_Button.Pages
 {
-    public class Box_PageModel : PageModel
+    public class boxModel : PageModel
     {
         public void OnGet()
         {
